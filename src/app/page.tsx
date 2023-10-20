8e517712bed38f6691468e2ca9a1d8050fc11cea
iCO22 <kosei20220709@outlook.jp>
2023-10-20 14:41:27 +0900

    second commit by iCO

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Image from 'next/image';
+import React from 'react';
+import './globals.css';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <h1>Hello</h1>
-    </main>
+    <React.Fragment>
+      <Image src="/vercel.svg" alt="unko" width={100} height={100}></Image>
+      <h1 className='hello'>Unkos</h1>
+    </React.Fragment>
   )
 }
